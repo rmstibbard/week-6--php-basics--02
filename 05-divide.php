80c25884b84c6055e1d0b7bc2f3892858411f405
Richard Stibbard <richardstibbard@gmail.com>
2020-05-02 14:15:12 +0100

    Divide - WORKING

---
+++ 05-divide.php
@@ -0,0 +1,15 @@
+<?php
+
+/* Create a function, divide, that takes two numbers as arguments. The function should return the first number divided by the second number. */
+
+declare(strict_types=1);
+
+function divide($a, $b)
+{
+    return ($a / $b);
+}
+
+var_dump(divide(4, 2)); // float(2)
+var_dump(divide(20, 2)); // float(10)
+var_dump(divide(5, 2)); // float(2.5)
+var_dump(divide(2.5, 0.5)); // float(5)
