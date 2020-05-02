0cf64076a1f7a735a12375f260f00baf213ceb16
Richard Stibbard <richardstibbard@gmail.com>
2020-05-02 16:12:16 +0100

    Wooooow - WORKING

---
+++ tricksy-01-wow.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+function wow($n)
+{
+    $output = "W";
+
+    for ($i = 1; $i <= $n; $i += 1) {
+        $output .= "o";
+    }
+
+    $output .= "w";
+    return $output;
+}
+
+var_dump(wow(12)); // string(14) "Woooooooooooow"
+var_dump(wow(4)); // string(6) "Woooow"
