3571e6f23d940519ce14d184d87e6d7a603d5cac
Richard Stibbard <richardstibbard@gmail.com>
2020-05-02 15:01:48 +0100

    Double - WORKING

---
+++ 11-double.php
@@ -0,0 +1,22 @@
+<?php
+
+/* Create a function, double, that takes an array of numbers as an argument. Return a new array containing each number doubled.
+*/
+
+declare(strict_types=1);
+
+function double($values)
+{
+    $doubled = [];
+
+    for ($i = 0; $i < count($values); $i += 1) {
+        array_push($doubled, ($values[$i] * 2));
+    }
+
+    return $doubled;
+};
+
+var_dump(
+    double([2, 3, 4, 5, 6]), // [4, 6, 8, 10, 12]
+    double([1, 2, 5]), // [2, 4, 10]
+);
